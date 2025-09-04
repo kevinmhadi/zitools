b462c0c30059f4a204ee464eb96913be90fb5fdc
kevinmhadi <kevinmhadi.gmail.com>
2025-09-04 12:08:50 -0400

    roxygen updates

---
+++ R/zitools-package.R
@@ -0,0 +1,18 @@
+#' @import Biostrings
+#' @import data.table
+#' @import GenomeInfoDb
+#' @import GenomicRanges
+#' @import ggplot2
+#' @import gTrack
+#' @import gGnome
+#' @import gUtils
+#' @import IRanges
+#' @import magrittr
+#' @importFrom VariantAnnotation readVcf info
+#' @importFrom MatrixGenerics rowRanges
+#' @importFrom BSgenome.Hsapiens.UCSC.hg19 Hsapiens
+#' @importFrom igraph graph.adjacency shortest_paths
+#' @importFrom khtools .gc setcols gr2df df2gr
+#' @importFrom rtracklayer import import.bw
+#' @importFrom skitools rel2abs ppng
+"_PACKAGE"
